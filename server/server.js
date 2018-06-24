04bb22ad86ae578a76657b36725b3f31b7ce5355
Martin Nazaryan <nazaryan.martin@outlook.comgit>
2018-06-24 22:03:15 +0400

    lesson server(http headers)

--- server/server.js
+++ server/server.js
@@ -14,11 +14,16 @@ const server = new http.Server(function (req, res) {
 
     if (parsedUrl.pathname === '/echo' && parsedUrl.query.message === 'Martin' && req.method === 'GET') {
 
+        // first method headers control( headers sends with response when called res.end() )
+        res.setHeader('cache-control', 'no-cache'); // disable cache
+
+        // second method , called immediately.
+        // res.writeHead(200, "OK", {"Cache-control": "no-cache"});
+
         res.end('Barev ' + parsedUrl.query.message);
 
     } else if (parsedUrl.pathname === '/favicon.ico') {
         res.end();
-
     } else {
         res.statusCode = 404;
         res.end('Error 404: Page not found');
