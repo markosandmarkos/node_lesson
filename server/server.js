f5678c022a9b976956c47843a6c6c5dae050b948
Martin Nazaryan <nazaryan.martin@outlook.comgit>
2018-06-24 21:26:58 +0400

    lesson server(url, method)

--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
+/*
+* Request to server
+* if "url = localhost/query?message=Martin"
+* then response to client that message,
+* else response message "page not found
+* */
+
 let http = require('http');
+let url = require('url');
 
-const server = new http.Server(); // EventEmitter
+const server = new http.Server(function (req, res) {
 
-server.listen(3005, '127.0.0.1');
+    let parsedUrl = url.parse(req.url, true);
 
-server.on('request', function (req, res) {
-    res.end('Barev');
-});
+    if (parsedUrl.pathname === '/echo' && parsedUrl.query.message === 'Martin') {
+
+        res.end(parsedUrl.query.message);
+
+    } else if (parsedUrl.pathname === '/favicon.ico') {
+        res.end();
+
+    } else {
+        res.statusCode = 404;
+        res.end('Error 404: Page not found');
+    }
+}); // EventEmitter
+
+server.listen(3001, '127.0.0.1');
+
