44965d07e4dfa3a4b2b58940cc305d0ca9bde5da
Martin Nazaryan <nazaryan.martin@outlook.comgit>
2018-06-26 00:36:45 +0400

    lesson async node (sync and async server examples)

---
+++ lesson_async_node/async.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+
+/**
+ * Sync example
+ * */
+
+// http.createServer(function (req, res) {
+//     let info;
+//     if(req.url === '/') {
+//         try {
+//             info = fs.readFileSync('lesson_async_node/info.json');
+//         } catch (err) {
+//             console.error(err);
+//             res.statusCode = 500;
+//             res.end("Internal server error");
+//             return;
+//         }
+//         res.end(info);
+//     }
+//
+// }).listen(3002);
+
+/**
+ * Async example
+ * */
+http.createServer(function (req, res) {
+
+    if (req.url === '/') {
+        fs.readFile('lesson_async_ode/info.json', function (err, info) {
+
+            if (err) {
+                console.error(err);
+                res.statusCode = 500;
+                res.end("Internal server error");
+                return;
+            }
+
+            res.end(info);
+            
+        });
+    }
+
+}).listen(3003);
